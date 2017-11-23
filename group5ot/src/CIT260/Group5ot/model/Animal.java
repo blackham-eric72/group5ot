b0d2d89a509f5e5f89c56a642c0818496ccd3094
ken <ken@USSAC2LT008.hka-global.com>
2017-11-22 19:32:49 -0700

    Added Animal model, AnimalType enum, Mods to AnimalHuntedControl

---
+++ group5ot/src/CIT260/Group5ot/model/Animal.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CIT260.Group5ot.model;
+
+/**
+ *
+ * @author ken
+ */
+public class Animal {
+    
+    private String name;
+    private double weight;
+
+    public Animal(String name, double weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" + "name=" + name + ", weight=" + weight + '}';
+    }
+    
+
+    
+}
