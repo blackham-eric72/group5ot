cadada20107998301e99c24facadd40abe4afbc3
Flores Family <Flores Family@DESKTOP-LAQB05T>
2017-11-24 16:00:48 -0700

    I ate some bug. I ate some grass.

--- group5ot/src/CIT260/Group5ot/control/ShoppingControl.java
+++ group5ot/src/CIT260/Group5ot/control/ShoppingControl.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package CIT260.Group5ot.control;
 
 import CIT260.Group5ot.enums.ShoppingListEnum;
@@ -15,42 +10,24 @@ import java.util.Scanner;
  * @author Flores Family
  */
 public class ShoppingControl {
-//BEGIN
-//total = 0
-//FOR index = 0 TO list.length -1
-// total = total + list[index]
-//ENDFOR
-//RETURN total
-//END
      
-    public static ArrayList<ShoppingListEnum> createList(){
+    public static ArrayList<ShoppingList> createList(){
                 
-        ArrayList<ShoppingListEnum> list = new ArrayList<>();
+        ArrayList<ShoppingList> list = new ArrayList<>();
         
         return list;
                     
     }
-    //for each item in the array of possible items they could purchase
-    //calculate the cost then add those together to get the total cost.
-    //How do I create my array and pass it to this function? the values
-    //are coming from buysuppliesview.
     
-    public double calculateTotalCost(double quantity, double price){
+    public double calculateTotalCost(ArrayList<ShoppingList> inList){
         double totalCost = 0;
 
-//        for (/*what do I put in here?*/) {
-//            if(quantity < 0){
-//                return -1;
-//            }
-//
-//            if(price < 0){
-//                return -1;
-//            }
-//
-//            double cost = quantity * price;
-//            totalCost += cost;
-//
-//        }
+        for (int i=0; i < inList.size();i++) {
+            double cost = inList.get(i).getQuantity() * inList.get(i).getPrice();
+            
+            totalCost += cost;
+
+        }
         return totalCost;
     }
 }
--- group5ot/src/CIT260/Group5ot/view/BuySuppliesView.java
+++ group5ot/src/CIT260/Group5ot/view/BuySuppliesView.java
@@ -1,133 +1,133 @@
-// /*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//package CIT260.Group5ot.view;
-//
-//import CIT260.Group5ot.control.ShoppingControl;
-//import java.util.Scanner;
-//import CIT260.Group5ot.control.TradingControl;
-//import CIT260.Group5ot.enums.ShoppingListEnum;
-//import CIT260.Group5ot.model.ShoppingList;
-//import static java.lang.Integer.parseInt;
-//import java.util.ArrayList;
-//
-///**
-// *
-// * @author crims
-// */
-//public class BuySuppliesView extends View {
-//       
-//       private String promptMessage; 
-//        
-//
-//        //Add end shopping option
-//        //Add loop to allow any number of items
-//        //Add subtotal after each purchase decision
-//        
-//    
-//        public BuySuppliesView() {
-//                    //display a welcome message and instructions and menu
-//            super( "\nWelcome to the Trading Post. "
-//                  +"\nEnter the item name, amount and price."
-//                  +"\nAt the current time, you must buy 2 items. "
-//                  + "\n|---------------------------------------|"
-//                  + "\n|              Buy Supplies             |"
-//                  + "\n|-------------------------------------- |"
-//                  + "\n|  S - Shop                             |"
-//                  + "\n|  Q - Return                           |"
-//                  + "\n|---------------------------------------|");
-//        }
-//        
-//    @Override
-//     public boolean doAction(String choice) {
-//         
-//            choice = choice.toUpperCase(); // convert to upper case
-//            
-//            switch (choice) {
-//                case "S": // display buy supplies menu
-//                    this.orderTotalCalculation();
-//                    break;
-//                case "Q": // return to previous screen
-//                    this.displayTradingpostMenuView();
-//                    break;
-//                default:
-//                   System.out.println("\n*** Invalid selection *** Try Again");
-//                   break;
-//        }
-//            return false;
-//            
-//     }
-//        
-//        
-//     public void orderTotalCalculation(){
-//        //Bring in ShoppingControl thingy. I don't know if I did this right.
-//        ShoppingControl createListFunction = new ShoppingControl(); 
-//                
-//        TradingControl calcTotalFunction = new TradingControl();
-//            //get purchase choices from user
-//            System.out.println("\n"
-//                + "\n|------------------------------------------------------|"       
-//                + "\n|                 Merchandise List                     |"
-//                + "\n|------------------------------------------------------|"
-//                + "\n|         Item                       Price             |" 
-//                + "\n|------------------------------------------------------|"
-//                + "\n|         Food                       $1.00 per pound   |"
-//                + "\n|         Ox                         $50.00            |"
-//                + "\n|         Ammo                       $0.05/round       |" 
-//                + "\n|         Medicine                   $0.05/dose        |"
-//                + "\n|------------------------------------------------------|"
-//            ); 
-//
-//        //get food quantity
-//        Scanner food = new Scanner(System.in);
-//
-//        System.out.println("Enter desired pounds of food. (0 if no food is needed)");
-//
-//        //create memory location for food
-//        int foodQuantity = parseInt(food.next());
-//        
-//        
-//        //get Ox quantity
-//        Scanner ox = new Scanner(System.in);
-//
-//        System.out.println("Enter desired number of oxen. (0 if no oxen are needed)");
-//
-//        //create memory location for ox
-//        int oxQuantity = parseInt(ox.next());
-//        
-//        
-//        //get Ammo quantity
-//        Scanner ammo = new Scanner(System.in);
-//
-//        System.out.println("Enter desired number of rounds of ammo. (0 if no ammo is needed)");
-//
-//        //create memory location for ammo
-//        int ammoQuantity = parseInt(ammo.next());
-//        
-//        
-//        //get medicine quantity
-//        Scanner medicine = new Scanner(System.in);
-//
-//        System.out.println("Enter desired number of doses of medicine. (0 if no medicine is needed)");
-//
-//        //create memory location for ammo
-//        int medicineQuantity = parseInt(medicine.next());
-//
-//        //here I would pass the variables I got up above to my control function which would then 
-//        //create the array and calculate the total cost with a for loop and the array. 
-//        //I don't think I did this right
-//        ShoppingList passFood = new ShoppingList("food", foodquantity, 1);
-//        ShoppingList passOx = new ShoppingList("ox", oxQuantity, 50);
-//        ShoppingList passAmmo = new ShoppingList("ammo", ammoQuantity, .05);
-//        ShoppingList passMedicine = new ShoppingList("medicine", medicineQuantity, .05);
-//    }
-//            
-//            
-//            
-//            double total = calcTotalFunction.calcTotal(itemAmountA, itemAmountB, itemPriceX, itemPriceY);
-//           System.out.println(
+package CIT260.Group5ot.view;
+
+import CIT260.Group5ot.control.ShoppingControl;
+import java.util.Scanner;
+import CIT260.Group5ot.control.TradingControl;
+import CIT260.Group5ot.enums.ShoppingListEnum;
+import CIT260.Group5ot.model.ShoppingList;
+import static java.lang.Integer.parseInt;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author crims
+ */
+public class BuySuppliesView extends View {
+       
+       private String promptMessage; 
+        
+
+        //Add end shopping option
+        //Add loop to allow any number of items
+        //Add subtotal after each purchase decision
+        
+    
+        public BuySuppliesView() {
+                    //display a welcome message and instructions and menu
+            super( "\nWelcome to the Trading Post. "
+                  +"\nEnter the item name, amount and price."
+                  +"\nAt the current time, you must buy 2 items. "
+                  + "\n|---------------------------------------|"
+                  + "\n|              Buy Supplies             |"
+                  + "\n|-------------------------------------- |"
+                  + "\n|  S - Shop                             |"
+                  + "\n|  Q - Return                           |"
+                  + "\n|---------------------------------------|");
+        }
+        
+    @Override
+     public boolean doAction(String choice) {
+         
+            choice = choice.toUpperCase(); // convert to upper case
+            
+            switch (choice) {
+                case "S": // display buy supplies menu
+                    this.orderTotalCalculation();
+                    break;
+                case "Q": // return to previous screen
+                    this.displayTradingpostMenuView();
+                    break;
+                default:
+                   System.out.println("\n*** Invalid selection *** Try Again");
+                   break;
+        }
+            return false;
+            
+     }
+        
+        
+     public void orderTotalCalculation(){
+        //Bring in ShoppingControl thingy. I don't know if I did this right.
+        ShoppingControl createListFunction = new ShoppingControl(); 
+                
+        TradingControl calcTotalFunction = new TradingControl();
+            //get purchase choices from user
+            System.out.println("\n"
+                + "\n|------------------------------------------------------|"       
+                + "\n|                 Merchandise List                     |"
+                + "\n|------------------------------------------------------|"
+                + "\n|         Item                       Price             |" 
+                + "\n|------------------------------------------------------|"
+                + "\n|         Food                       $1.00 per pound   |"
+                + "\n|         Ox                         $50.00            |"
+                + "\n|         Ammo                       $0.05/round       |" 
+                + "\n|         Medicine                   $0.05/dose        |"
+                + "\n|------------------------------------------------------|"
+            ); 
+
+        //get food quantity
+        Scanner food = new Scanner(System.in);
+
+        System.out.println("Enter desired pounds of food. (0 if no food is needed)");
+
+        //create memory location for food
+        int foodQuantity = parseInt(food.next());
+        
+        
+        //get Ox quantity
+        Scanner ox = new Scanner(System.in);
+
+        System.out.println("Enter desired number of oxen. (0 if no oxen are needed)");
+
+        //create memory location for ox
+        int oxQuantity = parseInt(ox.next());
+        
+        
+        //get Ammo quantity
+        Scanner ammo = new Scanner(System.in);
+
+        System.out.println("Enter desired number of rounds of ammo. (0 if no ammo is needed)");
+
+        //create memory location for ammo
+        int ammoQuantity = parseInt(ammo.next());
+        
+        
+        //get medicine quantity
+        Scanner medicine = new Scanner(System.in);
+
+        System.out.println("Enter desired number of doses of medicine. (0 if no medicine is needed)");
+
+        //create memory location for ammo
+        int medicineQuantity = parseInt(medicine.next());
+
+
+        ShoppingList passFood = new ShoppingList(ShoppingListEnum.food, foodQuantity, 1);
+        ShoppingList passOx = new ShoppingList(ShoppingListEnum.ox, oxQuantity, 50);
+        ShoppingList passAmmo = new ShoppingList(ShoppingListEnum.ammo, ammoQuantity, .05);
+        ShoppingList passMedicine = new ShoppingList(ShoppingListEnum.medicine, medicineQuantity, .05);
+    
+            
+            
+            
+        ArrayList<ShoppingList> myList = createListFunction.createList();
+		myList.add(passFood);
+		myList.add(passOx);
+		myList.add(passAmmo);
+		myList.add(passMedicine);
+		
+		double totalCost = createListFunction.calculateTotalCost(myList);           
+     
+//                System.out.println(
 //                              "\n--------------------------------------------------"
 //                            + "\n                Trading Post Receipt              "
 //                            + "\n--------------------------------------------------"
@@ -137,15 +137,17 @@
 //                            + "\n                                                  "        
 //                            + "\n Thank you for shopping at TrailMart. Come Again! "       
 //                            + "\n--------------------------------------------------");
-//                      
-//         }
-//
-//
-//    private void displayTradingpostMenuView() {
-//        TradingpostMenuView tradingpostMenuView = new TradingpostMenuView();
-//       
-//        tradingpostMenuView.display();
-//    }
-//           
-//
-//    
+    } 
+    
+     private void displayTradingpostMenuView() {
+        TradingpostMenuView tradingpostMenuView = new TradingpostMenuView();
+       
+        tradingpostMenuView.display();
+    }
+}
+
+
+
+           
+
+    
