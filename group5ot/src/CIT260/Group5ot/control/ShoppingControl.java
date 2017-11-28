479475fce1e9c871d83ff5bd86e8e3633d0e1aa6
Flores Family <Flores Family@DESKTOP-LAQB05T>
2017-11-27 20:26:53 -0700

    I am I am... a real religious man.

--- group5ot/src/CIT260/Group5ot/control/ShoppingControl.java
+++ group5ot/src/CIT260/Group5ot/control/ShoppingControl.java
@@ -1,6 +1,7 @@
 package CIT260.Group5ot.control;
 
 import CIT260.Group5ot.enums.ShoppingListEnum;
+import CIT260.Group5ot.exceptions.ShoppingControlException;
 import CIT260.Group5ot.model.ShoppingList;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -19,14 +20,23 @@ public class ShoppingControl {
                     
     }
     
-    public double calculateTotalCost(ArrayList<ShoppingList> inList){
+    public double calculateTotalCost(ArrayList<ShoppingList> inList) throws ShoppingControlException{
         double totalCost = 0;
 
         for (int i=0; i < inList.size();i++) {
-            double cost = inList.get(i).getQuantity() * inList.get(i).getPrice();
             
-            totalCost += cost;
+            if(inList.get(i).getQuantity() < 0|| inList.get(i).getQuantity() > 100){
+                throw new ShoppingControlException("Quantity must be greater than or equal to 0");
+            }
 
+            if(inList.get(i).getPrice()<0){
+                throw new ShoppingControlException("Price must be greater than or equal to 0");
+            }
+            else{
+                double cost = Math.round((inList.get(i).getQuantity() * inList.get(i).getPrice())*100)/100;
+                totalCost += cost;
+            }
+            
         }
         return totalCost;
     }
